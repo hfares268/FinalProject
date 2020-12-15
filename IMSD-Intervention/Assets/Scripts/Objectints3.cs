cb0c1bc52765d8fc3f654a0ea02a0342163a723f
Philip Green <Philip.Green@mail.citytech.cuny.edu>
2020-12-14 21:58:23 -0500

    Added anime facts

---
+++ IMSD-Intervention/Assets/Scripts/Objectints3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectints3 : MonoBehaviour
+{
+    public GameObject objectToInstantiate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            Instantiate(objectToInstantiate, transform.position + transform.forward, Quaternion.identity, transform);
+
+        }
+    }
+}
+
